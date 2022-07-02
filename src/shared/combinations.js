29f6b5dbe448967d3e73ff2bd289d242900ec138
Scheibelr <scheibelr@gmail.com>
2022-07-02 08:05:31 -0400

    added jpg store links to objects

--- src/shared/combinations.js
+++ src/shared/combinations.js
@@ -2,20 +2,6 @@ export const COMBINATIONS = [
     {
         id: 0,
         title: "Serpent Sage",
-        trait : {
-            image: ["images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png"],
-            name: ["Body Accessory: Familiar Serpent", "Mouth: Serpent",  "Marking: Serpent",  "Skin: Dragon Scales",  "Tail: Serpent" ],
-            altText: ["The Deadly Sins CNFT Trait: Body Accessory: Familiar Serpent", "The Deadly Sins CNFT Trait: Mouth: Serpent",  "The Deadly Sins CNFT Trait: Marking: Serpent",  "The Deadly Sins CNFT Trait: Skin: Dragon Scales",  "The Deadly Sins CNFT Trait: Tail: Serpent"],
-            jpgStoreLink: [
-                "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522body%2520accessory%2522%253A%255B%2522familiar%2520-%2520serpent%2522%255D%257D",
-            ]
-        },
-        backgroundImg: "images/orange-fire.png",
-        tokens: 6,
-    },
-    {
-        id: 0,
-        title: "Serpent Sage",
         trait : [
             {
             traitId: 0,
@@ -29,28 +15,28 @@ export const COMBINATIONS = [
             image: "images/body-accessory-familiar-serpent.png", 
             name: "Mouth: Serpent",
             altText: "The Deadly Sins CNFT Trait: Mouth: Serpent",
-            jpgStoreLink: ""
+            jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522mouth%2522%253A%255B%2522serpent%2522%255D%257D",
         },
         {
             traitId: 2,
             image: "", 
             name: "Marking: Serpent",
             altText: "The Deadly Sins CNFT Trait: Marking: Serpent",
-            jpgStoreLink: ""
+            jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522marking%2522%253A%255B%2522serpent%2522%255D%257D",
         },
         {
             traitId: 3,
             image: "", 
             name: "Skin: Dragon Scales",
             altText:  "The Deadly Sins CNFT Trait: Skin: Dragon Scales",
-            jpgStoreLink: ""
+            jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522skin%2520type%2522%253A%255B%2522dragon%2520scales%2522%255D%257D",
         },
         {
             traitId: 4,
             image: "", 
             name: "Tail: Serpent",
             altText: "The Deadly Sins CNFT Trait: Tail: Serpent",
-            jpgStoreLink: ""
+            jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522tail%2522%253A%255B%2522serpent%2522%255D%257D"
         },
     ]
 
