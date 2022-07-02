2776943e016e2401192a0532903aaaaef6fd70f8
Scheibelr <scheibelr@gmail.com>
2022-07-02 08:02:19 -0400

    converted trait object of arrays to array of objects

--- src/components/CombinationComponent.js
+++ src/components/CombinationComponent.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Card, CardText, CardImg, Row, Col } from 'reactstrap';
+import './CombinationComponent.scss'
 function Combination(props) {
     const myCombination = props.combinations.map(item => {
         return (
         <Col key={item.id} xs={{size:4}}>
             <Card>
             {/* <a href={item.link}> */}
-                <CardImg onClick={() => {props.projectSelect(item.id)}} src={item.coverImg} alt={item.trait.altText}></CardImg>
+            <a href={item.trait.jpgStoreLink}>
+                <CardImg src={item.trait.image} alt={item.trait.altText}></CardImg>
+            </a>
             {/* </a> */}
                 <CardText className="card-title">
-                    {item.title}
+                    {item.trait.name}
                 </CardText>
             </Card>
         </Col>
         )
     })
+
     return (
         <>
             <Row className="card-row">
--- src/shared/combinations.js
+++ src/shared/combinations.js
@@ -3,13 +3,57 @@ export const COMBINATIONS = [
         id: 0,
         title: "Serpent Sage",
         trait : {
-            traitImage: ["images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png"],
-            traitName: ["Body Accessory: Familiar Serpent", "Mouth: Serpent",  "Marking: Serpent",  "Skin: Dragon Scales",  "Tail: Serpent" ],
+            image: ["images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png", "images/body-accessory-familiar-serpent.png"],
+            name: ["Body Accessory: Familiar Serpent", "Mouth: Serpent",  "Marking: Serpent",  "Skin: Dragon Scales",  "Tail: Serpent" ],
             altText: ["The Deadly Sins CNFT Trait: Body Accessory: Familiar Serpent", "The Deadly Sins CNFT Trait: Mouth: Serpent",  "The Deadly Sins CNFT Trait: Marking: Serpent",  "The Deadly Sins CNFT Trait: Skin: Dragon Scales",  "The Deadly Sins CNFT Trait: Tail: Serpent"],
             jpgStoreLink: [
                 "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522body%2520accessory%2522%253A%255B%2522familiar%2520-%2520serpent%2522%255D%257D",
             ]
         },
+        backgroundImg: "images/orange-fire.png",
+        tokens: 6,
+    },
+    {
+        id: 0,
+        title: "Serpent Sage",
+        trait : [
+            {
+            traitId: 0,
+            image: "images/body-accessory-familiar-serpent.png",
+            name: "Body Accessory: Familiar Serpent",    
+            altText: "The Deadly Sins CNFT Trait: Body Accessory: Familiar Serpent",
+            jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522body%2520accessory%2522%253A%255B%2522familiar%2520-%2520serpent%2522%255D%257D",
+        },
+        {
+            traitId: 1,
+            image: "images/body-accessory-familiar-serpent.png", 
+            name: "Mouth: Serpent",
+            altText: "The Deadly Sins CNFT Trait: Mouth: Serpent",
+            jpgStoreLink: ""
+        },
+        {
+            traitId: 2,
+            image: "", 
+            name: "Marking: Serpent",
+            altText: "The Deadly Sins CNFT Trait: Marking: Serpent",
+            jpgStoreLink: ""
+        },
+        {
+            traitId: 3,
+            image: "", 
+            name: "Skin: Dragon Scales",
+            altText:  "The Deadly Sins CNFT Trait: Skin: Dragon Scales",
+            jpgStoreLink: ""
+        },
+        {
+            traitId: 4,
+            image: "", 
+            name: "Tail: Serpent",
+            altText: "The Deadly Sins CNFT Trait: Tail: Serpent",
+            jpgStoreLink: ""
+        },
+    ]
+
         backgroundImg: "images/orange-fire.png",
         tokens: 6,
     },
