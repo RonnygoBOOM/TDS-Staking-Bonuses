4b983c0903c2b91c73d9f8c8f84f7ce7c371d23b
Scheibelr <scheibelr@gmail.com>
2022-07-02 08:16:35 -0400

    mapped trait-items

--- src/components/CombinationComponent.js
+++ src/components/CombinationComponent.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Card, CardText, CardImg, Row, Col } from 'reactstrap';
-import './CombinationComponent.scss'
+
 function Combination(props) {
     const myCombination = props.combinations.map(item => {
         return (
-        <Col key={item.id} xs={{size:4}}>
-            <Card>
-            {/* <a href={item.link}> */}
-            <a href={item.trait.jpgStoreLink}>
-                <CardImg src={item.trait.image} alt={item.trait.altText}></CardImg>
-            </a>
-            {/* </a> */}
-                <CardText className="card-title">
-                    {item.trait.name}
-                </CardText>
-            </Card>
-        </Col>
+            <Row className="card-row" key={item.id}>
+                {item.trait.map(traitItem => {
+                    return (
+                        <Col className="card-column" key={traitItem.traitId} xs={{size:2}}>
+                            <Card>
+                                
+                                <CardText>
+                                    {traitItem.name}
+                                </CardText>
+                            </Card>
+                        </Col>
+                    )
+                }
+                    
+                    
+                    )}
+            </Row>
+        // <Col key={item.id} xs={{size:4}}>
+        //     <Card>
+        //     {/* <a href={item.link}> */}
+        //     <a href={item.trait.jpgStoreLink}>
+        //         <CardImg src={item.trait.image} alt={item.trait.altText}></CardImg>
+        //     </a>
+        //     {/* </a> */}
+        //         <CardText className="card-title">
+        //             {item.trait.name}
+        //         </CardText>
+        //     </Card>
+        // </Col>
         )
     })
+
 
     return (
         <>
--- src/shared/combinations.js
+++ src/shared/combinations.js
@@ -38,7 +38,7 @@ export const COMBINATIONS = [
             altText: "The Deadly Sins CNFT Trait: Tail: Serpent",
             jpgStoreLink: "https://www.jpg.store/collection/thedeadlysins?filters=%257B%2522tail%2522%253A%255B%2522serpent%2522%255D%257D"
         },
-    ]
+    ],
 
         backgroundImg: "images/orange-fire.png",
         tokens: 6,
