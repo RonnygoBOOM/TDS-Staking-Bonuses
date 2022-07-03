657c66a417cafa1f73373fafe57e777ebf5c0d45
Scheibelr <scheibelr@gmail.com>
2022-07-02 22:57:11 -0400

    started row and column css and combinations object intigration

--- src/components/CombinationComponent.js
+++ src/components/CombinationComponent.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import { Card, CardText, CardImg, Row, Col } from 'reactstrap';
+import React from "react";
+import { Card, CardText, CardImg, Row, Col } from "reactstrap";
 
 function Combination(props) {
-    const myCombination = props.combinations.map(item => {
-        return (
-            <Row className="card-row" key={item.id}>
-                {item.trait.map(traitItem => {
-                    return (
-                        <Col className="card-column" key={traitItem.traitId} xs={{size:2}}>
-                            <Card>
-                                
-                                <CardText>
-                                    {traitItem.name}
-                                </CardText>
-                            </Card>
-                        </Col>
-                    )
-                }
-                    
-                    
-                    )}
-            </Row>
-        // <Col key={item.id} xs={{size:4}}>
-        //     <Card>
-        //     {/* <a href={item.link}> */}
-        //     <a href={item.trait.jpgStoreLink}>
-        //         <CardImg src={item.trait.image} alt={item.trait.altText}></CardImg>
-        //     </a>
-        //     {/* </a> */}
-        //         <CardText className="card-title">
-        //             {item.trait.name}
-        //         </CardText>
-        //     </Card>
-        // </Col>
-        )
-    })
-
-
+  const myCombination = props.combinations.map((item) => {
     return (
-        <>
-            <Row className="card-row">
-                {myCombination}
-            </Row>
-        </>
+      <Row className="card-row" key={item.id}>
+        <Col xs={{ size: 1 }}>
+          <h3>{item.title}</h3>
+        </Col>
+        {item.trait.map((traitItem) => {
+          return (
+            <Col
+              className="card-column"
+              key={traitItem.traitId}
+              xs={{ size: 1 }}
+            >
+              <a href={traitItem.jpgStoreLink}>
+                <Card>
+                  <CardImg
+                    src={traitItem.image}
+                    alt={traitItem.altText}
+                  ></CardImg>
+                  <CardText>{traitItem.name}</CardText>
+                </Card>
+              </a>
+            </Col>
+          );
+        })}
+        <Col xs={{ size: 1 }}>
+          <h3>{item.tokens}</h3>
+        </Col>
+      </Row>
+      // <Col key={item.id} xs={{size:4}}>
+      //     <Card>
+      //     {/* <a href={item.link}> */}
+      //   <a href={item.trait.jpgStoreLink}>
+      //       <CardImg src={item.trait.image} alt={item.trait.altText}></CardImg>
+      //   </a>
+      //     {/* </a> */}
+      //         <CardText className="card-title">
+      //             {item.trait.name}
+      //         </CardText>
+      //     </Card>
+      // </Col>
     );
+  });
+
+  return (
+    <>
+      <Row className="card-row">{myCombination}</Row>
+    </>
+  );
 }
 
 export default Combination;
