ddcd9155f9872652f0a72462fe4b5043d36628ba
Scheibelr <scheibelr@gmail.com>
2022-07-06 17:04:50 -0400

    css token fixed and top styled

--- src/components/CombinationComponent.js
+++ src/components/CombinationComponent.js
@@ -33,7 +33,15 @@ function Combination(props) {
         })}
         </div>
         <Col className="title-column" xs={{ size: 1 }}>
-          <h3 className="title-text">{item.tokens}</h3>
+            <Container className="token-container">
+            <Col xs={{size: 12 }} className="token-row">
+          <h3 className="title-text">{item.tokens + " sin tokens"}</h3>
+          </Col>
+          <Col xs={{size: 12 }} className="token-row">
+            {/* earring is an image placeholder until I get the token image.*/}
+          <img className="token-image" alt="placeholder sin token" src="images/earring/cardano-bling.png"></img>
+          </Col>
+          </Container>
         </Col>
       </Row>
       
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,9 +5,11 @@ import Combination from './CombinationComponent';
 function Main() {
     return (
 <>
-<p>Hello Sinners!</p>
-<p>This is a simple app to help you hunt for traits. Click on any of the traits to view it on jpg.store</p>
+<div style={{color: "rgb(250, 142, 0)", backgroundColor: "black"}}>
+<p style={{margin: "3em"}}>Hello Sinners!</p>
+<p style={{margin: "3em"}}>This is a simple app to help you hunt for traits. Click on any of the traits to view it on jpg.store</p>
 <Combination combinations={COMBINATIONS}/>
+</div>
 </>
     );
 }
