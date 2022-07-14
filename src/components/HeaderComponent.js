2e5c93b336a73ec6085fbfa0bb601b38aa7c6862
RonnygoBOOM <scheibelr@gmail.com>
2022-07-13 21:15:13 -0500

    wip nav with dropdown link for each role

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,16 +1,65 @@
 import React from 'react';
-import {Nav, NavItem} from 'reactstrap';
+import {Navbar, Collapse, NavbarBrand, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler, Nav, NavItem, NavLink} from 'reactstrap';
 
-function Header() {
+function Header(props) {
+    const [navCollapse, setNavCollapse] = React.useState(false)
+    const [dropdownOpen, setDropdownOpen] = React.useState(false)
+
+    const dropdownRole = props.combinations.map((item) => {
+        return (
+            <>
+                <DropdownItem>
+                    <a href={`"/#" + ${item.title}`} alt={item.alt}>
+                    {item.title}
+                    </a>
+                </DropdownItem>
+            </>
+        );
+    })
+    function toggle() {
+        setDropdownOpen(prev => !prev)
+    }
+
+    function toggleNavbarCollapse() {
+        setNavCollapse(prev => !prev)
+    }
+
+
     return (
-<>
-<Nav>
-    <NavItem>
-        The Deadly Sins
-    </NavItem>
-</Nav>
-</>
+            <Navbar dark sticky="top" expand="md">
+                {/* <img className="logo" src="/images/cardano-ada-logo.png"></img>
+                <h1>cnftTOP</h1> */}
+                <NavbarBrand>The Deadly Sins</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbarCollapse} />
+                <Collapse isOpen={navCollapse} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle caret>
+                            Roles
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            {dropdownRole}
+                        </DropdownMenu>
+                    </Dropdown>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/#">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/#experiments">Experiments</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/#about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/#contact">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar> 
     );
 }
-
 export default Header;
+
+
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {COMBINATIONS} from '../shared/combinations';
 import Combination from './CombinationComponent';
-import Header from './HeaderComponent';
+// import Header from './HeaderComponent';
 
 function Main() {
     return (
 <>
-<Header />
+{/* <Header combinations={COMBINATIONS}/> */}
 <div style={{color: "rgb(250, 142, 0)", backgroundColor: "black"}}>
 <p style={{margin: "3em"}}>Hello Sinners!</p>
 <p style={{margin: "3em"}}>This is a simple app to help you hunt for traits. Click on any of the traits to view them on jpg.store</p>
