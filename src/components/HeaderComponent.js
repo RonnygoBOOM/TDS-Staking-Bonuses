6fee9cd8da8494b3ec0523a267946c82190cfc79
RonnygoBOOM <scheibelr@gmail.com>
2022-07-19 22:14:52 -0500

    nav styles and text styles fixed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import {Navbar, Collapse, NavbarBrand, Dropdown, DropdownToggle, DropdownMenu, NavbarToggler, Nav, NavItem} from 'reactstrap';
+import {Navbar, NavbarBrand } from 'reactstrap';
+// import {Collapse, Dropdown, DropdownToggle, DropdownMenu, NavbarToggler, Nav, NavItem } from 'reactstrap';
 
 function Header(props) {
-    const [navCollapse, setNavCollapse] = React.useState(false)
-    const [dropdownOpen, setDropdownOpen] = React.useState(false)
+    // const [navCollapse, setNavCollapse] = React.useState(false)
+    // const [dropdownOpen, setDropdownOpen] = React.useState(false)
 
-    const dropdownRole = props.combinations.map((item) => {
-        return (
-                <div className="roles-item" onClick={toggleDropdown}>
-                    <a className="roles-item-link" href={`/#${item.title}`} alt={item.alt}>
-                    {item.title}
-                    </a>
-                </div>
-        );
-    })
-    function toggleDropdown() {
-        setDropdownOpen(prev => !prev)
-    }
+    // const dropdownRole = props.combinations.map((item) => {
+    //     return (
+    //             <div className="roles-item" onClick={toggleDropdown}>
+    //                 <a className="roles-item-link" href={`/#${item.title}`} alt={item.alt}>
+    //                 {item.title}
+    //                 </a>
+    //             </div>
+    //     );
+    // })
+    // function toggleDropdown() {
+    //     setDropdownOpen(prev => !prev)
+    // }
 
-    function toggleNavbarCollapse() {
-        setNavCollapse(prev => !prev)
-    }
+    // function toggleNavbarCollapse() {
+    //     setNavCollapse(prev => !prev)
+    // }
 
 
     return (
             <Navbar sticky="top" expand="md">
+                <div style={{display: 'flex', justifyContent:'center', alignContent: 'center'}}>
                 <NavbarBrand>The Deadly Sins</NavbarBrand>
-                <NavbarToggler onClick={toggleNavbarCollapse} />
+                </div>
+                <div style={{display: 'flex', justifyContent:'left'}}>
+            <img src="favicon.ico" alt='the deadly sins nft sample number 8'className="nav-tds-icon"></img>
+            </div>
+                {/* <NavbarToggler onClick={toggleNavbarCollapse} />
                 <Collapse isOpen={navCollapse} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
@@ -41,7 +47,7 @@ function Header(props) {
                         <DropdownMenu className="roles-menu">
                             {dropdownRole}
                         </DropdownMenu>
-                    </Dropdown>
+                    </Dropdown> */}
             </Navbar> 
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {COMBINATIONS} from '../shared/combinations';
 import Combination from './CombinationComponent';
 import NFTScroll from './NFTScrollComponent';
-// import Header from './HeaderComponent';
+import Header from './HeaderComponent';
 
 function Main() {
     return (
 <>
-<NFTScroll/>
-{/* <Header combinations={COMBINATIONS}/> */}
+<Header combinations={COMBINATIONS}/>
 <div style={{color: "rgb(250, 142, 0)", backgroundColor: "black"}}>
-<p style={{margin: "3em"}}>Hello Sinners!</p>
-<p style={{margin: "3em"}}>This is a simple app to help you hunt for traits. Click on any of the traits to view them on jpg.store</p>
+<p className="main-text">Hello Sinners!</p>
+<p className="main-text">This is a simple app to help you hunt for traits. Click on any of the traits to view them on jpg.store</p>
+<NFTScroll/>
 <Combination combinations={COMBINATIONS}/>
-<p style={{margin: "3em", textAlign: 'center'}}>Please contact me on twitter @RonnygoBOOM if you find any broken links or have suggestions for improvement!</p>
+<p className="main-text" style={{ textAlign: 'center'}}>Please contact me on twitter @RonnygoBOOM if you find any broken links or have suggestions for improvement!</p>
 </div>
 </>
     );
